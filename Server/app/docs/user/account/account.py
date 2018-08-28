3cbde1b8fa535a5736015dfec6ce9d11c1f8cfba
miraedbswo <miraedbswo@gmail.com>
2018-08-28 23:52:50 +0900

    ADD SWAGGER TEMPLETE

---
+++ Server/app/docs/user/account/account.py
@@ -0,0 +1,34 @@
+from app.docs import jwt_header
+
+SAMPLE_ACCOUNT = {
+    'tags': [''],
+    'description': '',
+    'parameters': [
+        jwt_header,
+        {
+            'name': '',
+            'description': '',
+            'in': '',
+            'type': '',
+            'required': None
+        }
+    ],
+    'responses': {
+        '200': {
+            'description': '',
+            'examples': {
+                '': {
+                    '': ''
+                }
+            }
+        },
+        '201': {
+            'description': '',
+            'examples': {
+                '': {
+                    '': ''
+                }
+            }
+        }
+    }
+}
